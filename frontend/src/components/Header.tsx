54828a3f1ac7c5d2d6199c53ed8330adcd60949b
dilip-d <dilipdofficial@gmail.com>
2024-10-18 01:57:51 +0530

    change in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -37,7 +37,7 @@ const Header: React.FC = () => {
         className="text-xl font-bold italic cursor-pointer"
         onClick={() => navigate("/")}
       >
-        Patient Management System
+        Healthcare Management System
       </h1>
       <button
         onClick={toggleMenu}
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
-// const API_URL = "https://dashboard-4mmw.onrender.com/api";
+// const API_URL = "http://localhost:5000/api";
+const API_URL = "https://dashboard-4mmw.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: API_URL,
